a2d61a243373b534262eb8d554d6e69b4d21ad4c
Fellipe Azambuja <femontanhanew@gmail.com>
2019-12-01 22:01:53 -0300

    Todo react :)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import cx from 'classnames';
 import './App.css';
 
-function App() {
+function Todo({ todo, index, completeTodo }) {
+  const css = cx('todo', { 'is-done': todo.isCompleted });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={ css } onClick={ () => completeTodo(index, todo.isCompleted) } >
+      { todo.text }
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [ value, setValue ] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form className="form" onSubmit={ handleSubmit }>
+      <input
+        type="text"
+        className="form__input"
+        value={ value }
+        onChange={ e => setValue(e.target.value) }
+        placeholder='Add todo...'
+      />
+    </form>
+  );
+}
+
+function TodoCount({ todos }) {
+  const count = todos.reduce((accum, todo) => (todo.isCompleted ? accum : accum + 1), 0);
+
+  return (
+    <div className="count">{count} task(s)</div>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Minha primeira task',
+    },
+    {
+      text: 'Minha segunda task',
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (index, isCompleted) => {
+    const newTodos = [...todos];
+
+    if (isCompleted) {
+      newTodos[index].isCompleted = false;
+    } else {
+      newTodos[index].isCompleted = true;
+    }
+
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="app">
+      <TodoCount todos={ todos }/>
+      <TodoForm addTodo={ addTodo } />
+      {todos.map((todo, index) => (
+        <Todo
+          key={ index }
+          index={ index }
+          todo={ todo }
+          completeTodo={ completeTodo }
+        />
+      ))}
     </div>
   );
 }
